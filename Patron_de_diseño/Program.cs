0d9cba36cdb369f403da0b44469e082319ccd8b5
JoshuaBasto184 <56733874+JoshuaBasto184@users.noreply.github.com>
2019-12-26 10:58:06 -0600

    proyecto eventos

---
+++ Patron_de_diseño/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+namespace Patron_de_diseño
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string direccion = File.ReadAllText(@"C:\Users\JoshuaBasto\Desktop\PD.txt");
+            string[] separadas;
+            DateTime fechaActual = DateTime.Now;
+            DateTime fecha;
+            int diferenciasDias;
+
+            separadas = direccion.Split(',');//hace la separación en la cadena por medio de coma
+
+            fecha = Convert.ToDateTime(separadas[1]);// convierte el dato de especifico en formato fecha
+
+            TimeSpan ts = fechaActual - fecha;
+
+            // Difference in days.
+            diferenciasDias = ts.Days;
+
+            //Compara la fecha 
+            if (fecha > fechaActual)
+            {
+                Console.WriteLine("{0} ocurría dentro de {1}", separadas[0], diferenciasDias);
+            }
+            else if (fecha < fechaActual)
+            {
+                Console.WriteLine("{0} ocurrió hace {1}", separadas[0], diferenciasDias);
+            }
+            else
+            {
+                Console.WriteLine("{0} es hoy", separadas[0]);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
